2d8aa5aee675777dc7f1ce2a98a9c53635fb688b
71wx <71wx@mail85.com>
2017-10-12 11:52:31 +0800

    Ini

---
+++ js/phpmailer.js
@@ -0,0 +1,60 @@
+/**
+ * 发送邮件验证
+ * @author lxl
+ */
+var PHPMailer={
+    /**
+     * 发送邮件
+     */
+    'sendEmail' : function() {
+        $.ajax(
+            {
+                type: "GET",
+                url : "http://hostidc.com/mail/send71wx.php",
+                data: {
+                    'userEmail'		 : $("#userEmail").val(),
+                    'userName'		 : $("#userName").val(),
+                    'userTitle'      : $("#userTitle").val(),
+                    'userMsg'		 : $("#userMsg").val()
+                },
+                async: false,
+                dataType: "jsonp",
+				jsonp: "callback",
+				jsonpCallback:"sendEmailHandler",
+                beforeSend: function (xhr) {
+                    //pjax 实现
+                    //xhr.setRequestHeader('X-PJAX', 'true');
+
+                    /** 邮件发送中 **/
+                    $("#send_email").css('display','none');
+                    $("#loading").show();
+                },
+                success: function (result) {
+                    //pjax 实现
+                    //history.pushState(null, '', '/PHPMailer-Demo');
+
+                    $("#loading").hide();
+                    if (result.code==='ok') {
+                        /** 发送成功 **/
+                        $("#alert-success").show();
+                        $("#alert-danger").hide();
+                    } else {
+	                    $("#send_email").show();
+                        /** 发送失败 **/
+                        $("#alert-success").hide();
+                        $("#alert-danger").show();
+                    }
+                }
+            });
+    }
+};
+
+$(function(){
+    $("#send_email").click(function(){
+        ///** 验证 **/
+        //if (PHPMailer.checkEmpty()) {
+            /** 发送邮件 **/
+            PHPMailer.sendEmail();
+        //}
+    });
+});
